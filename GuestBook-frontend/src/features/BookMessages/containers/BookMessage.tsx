5e4928fdfa82402e930bc18e326683a7a168857f
DarkFlarex <darkflarexxx@gmail.com>
2024-08-16 03:49:01 +0600

    Обновлён компонент BookMessage.Теперь отображает список сообщений с использованием компонента BookMessageItem.

--- GuestBook-frontend/src/features/BookMessages/containers/BookMessage.tsx
+++ GuestBook-frontend/src/features/BookMessages/containers/BookMessage.tsx
@@ -1,23 +1,51 @@
 import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
-import {selectBookMessageCreating } from "../store/bookMessagesSlice.ts";
+import {selectBookMessageCreating, selectBookMessages, selectBookMessagesFetching} from "../store/bookMessagesSlice.ts";
 import {BookMessageMutation} from "../../../types.ts";
-import {createBookMessage} from "../store/bookMessagesThunks.ts";
+import {createBookMessage, fetchBookMessages} from "../store/bookMessagesThunks.ts";
 import BookMessageForm from "../components/BookMessageForm.tsx";
-
+import {CircularProgress, Grid} from "@mui/material";
+import BookMessageItem from "../components/BookMessageItem.tsx";
+import {useEffect} from "react";
 
 const BookMessage = () => {
     const dispatch = useAppDispatch();
     const isCreating = useAppSelector(selectBookMessageCreating);
+    const BookMessages = useAppSelector(selectBookMessages);
+    const isFetching = useAppSelector(selectBookMessagesFetching);
 
+    useEffect(() => {
+        dispatch(fetchBookMessages());
+        const interval = setInterval(() => {
+            dispatch(fetchBookMessages());
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [dispatch]);
 
     const onFormSubmit = async (bookMessageMutation:BookMessageMutation) =>{
         await dispatch(createBookMessage(bookMessageMutation));
     };
 
-
     return (
         <>
             <BookMessageForm onSubmit={onFormSubmit} isLoading={isCreating}/>
+            <Grid container direction="column" spacing={2}
+                  sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      mb: '200px' }}
+            >
+                {isFetching && <Grid item> <CircularProgress/> </Grid>}
+                {BookMessages.map(bookMessage => (
+                    <BookMessageItem
+                        key={bookMessage.id}
+                        author={bookMessage.author}
+                        message={bookMessage.message}
+                        image={bookMessage.image}
+                    />
+                ))}
+            </Grid>
         </>
     );
 };
